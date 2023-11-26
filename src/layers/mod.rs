9ddb79d7d2551e9c9d97fbaa884c42c81a4e7768
rfast <rfast@expediagroup.com>
2023-11-25 18:57:02 -0600

    remove variables

--- src/layers/mod.rs
+++ src/layers/mod.rs
@@ -1,5 +1,4 @@
 use crate::error::Result;
-use crate::variables::Variable;
 use candle_core::{DType, Device, Module, Shape, Tensor, Var, WithDType};
 use candle_nn::init::{DEFAULT_KAIMING_UNIFORM, ZERO};
 use candle_nn::{Init, VarBuilder};
@@ -12,31 +11,4 @@ pub trait Layer: candle_core::Module {
 
 pub trait LayerBuilder<L: Layer> {
     fn build(&self, input_shape: &Shape, vs: &VarBuilder) -> Result<L>;
-}
-
-fn create_variable(
-    name: &str,
-    shape: &Shape,
-    initializer: Option<Init>,
-    dtype: DType,
-    trainable: bool,
-    device: &Device,
-) -> Result<Variable> {
-    let initializer = initializer.unwrap_or(
-        // todo Should this be Glorot?
-        if dtype.is_float() {
-            DEFAULT_KAIMING_UNIFORM
-        } else {
-            ZERO
-        },
-    );
-
-    Ok(Variable::new(
-        initializer,
-        shape,
-        dtype,
-        trainable,
-        name.to_string(),
-        device,
-    )?)
 }
